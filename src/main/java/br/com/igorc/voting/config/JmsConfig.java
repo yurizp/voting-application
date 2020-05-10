e653a2e15601b6c127650200b848d822a06c5876
Igor Ferreira Cemim <igor@cemim.com.br>
2020-05-10 03:07:50 -0300

    Adicionadas configurações para mensageria e agendamento de tarefas

---
+++ src/main/java/br/com/igorc/voting/config/JmsConfig.java
@@ -0,0 +1,36 @@
+package br.com.igorc.voting.config;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+	private static final String CONFIG_CONCURRENCY = "2-10";
+
+	@Bean
+	public JmsListenerContainerFactory<?> defaultContainerFactory(ConnectionFactory connectionFactory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		factory.setConcurrency(CONFIG_CONCURRENCY);
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+}
---
+++ src/main/java/br/com/igorc/voting/config/TaskSchedulerConfig.java
@@ -0,0 +1,18 @@
+package br.com.igorc.voting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class TaskSchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        threadPoolTaskScheduler.setThreadNamePrefix("votingPool");
+        return threadPoolTaskScheduler;
+    }
+
+}
